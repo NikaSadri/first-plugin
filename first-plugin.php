df3ddd9d716885076179ffe3b6f352d07a9b39de
NikaSadri <111984672+NikaSadri@users.noreply.github.com>
2024-04-17 14:33:32 +0330

    Add urgent comment
    Add urgent comment at the top of the main file.

---
+++ first-plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: First Plugin
+Plugin URI: https://+++/
+Description: It's Nika's first plugin.
+Version: 0.1
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: Nika
+Author URI: +++/
+License: GPLv2 or later
+Text Domain: first-plugin
+*/
